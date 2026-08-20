dd96933e57f02ee4189af92e93339765710bbee1
jasonloh <lohks6536@gmail.com>
2026-08-20 17:40:07 +0800

    feat(log) : add logging

--- src/main/java/com/project/ks/gmc_discord_bot/GmcDiscordBotApplication.java
+++ src/main/java/com/project/ks/gmc_discord_bot/GmcDiscordBotApplication.java
@@ -1,5 +1,7 @@
 package com.project.ks.gmc_discord_bot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,8 +10,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class GmcDiscordBotApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(GmcDiscordBotApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(GmcDiscordBotApplication.class, args);
+		LOGGER.info("Application Startup Successfully");
 	}
 
 }
